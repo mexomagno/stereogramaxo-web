c23b1ff11a2f13b706e160d302651b6a3524aa66
Maximiliano Castro <max@djavu.cl>
2021-01-04 23:00:25 -0300

    minor

---
+++ src/components/Stereogram.vue
@@ -0,0 +1,15 @@
+<template>
+  <div></div>
+</template>
+<script>
+export default {
+  name: 'Stereogram',
+  data () {
+    return {
+    }
+  }
+}
+</script>
+
+<style lang="css" scoped>
+</style>
